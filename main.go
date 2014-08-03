f711cf34011c544bc5e668ab192776b2b2fcf5b7
Travis Jeffery <tj@travisjeffery.com>
2014-08-03 01:59:27 -0400

    Add installLaunchAgent()

--- main.go
+++ main.go
@@ -4,7 +4,9 @@ import "os"
 import . "github.com/visionmedia/go-debug"
 import "github.com/codegangsta/cli"
 import "github.com/travisjeffery/tron/cmd"
+import "github.com/DHowett/go-plist"
 import "fmt"
+import "log"
 
 var debug = Debug("tron")
 
@@ -32,14 +34,46 @@ func main() {
 			Usage: "Schedules tron to report daily",
 			Action: func(c *cli.Context) {
 				debug("tron install")
-
-				var plist = "$HOME/Library/LaunchAgents/com.travisjeffery.tron"
-
-				if _, err := os.Stat(plist); err != nil {
-					cmd.New(fmt.Sprintf("launchtl unload \"%s\"", plist))
-				}
+				installLaunchAgent()
 			},
 		},
 	}
 	app.Run(os.Args)
+}
+
+func installLaunchAgent() {
+	label := "com.travisjeffery.tron"
+	p := fmt.Sprintf("$HOME/Library/LaunchAgents/%s", label)
+
+	if _, err := os.Stat(p); err != nil {
+		cmd.New(fmt.Sprintf("launchtl unload \"%s\"", p)).Exec()
+	}
+
+	f, err := os.Create(p)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	encoder := plist.NewEncoder(f)
+	encoder.Encode(map[string]string{
+		"Label":             label,
+		"StandardOutPath":   "",
+		"StandardErrorPath": "",
+	})
+	encoder.Encode(map[string][]string{
+		// tron needs to be the path to the tron executable here
+		"ProgramArguments": []string{"tron", "report"},
+	})
+	encoder.Encode(map[string]map[string]int{
+		"StartCalendarInterval": map[string]int{
+			"Hour":   15,
+			"Minute": 0,
+		},
+	})
+
+	cmd.New(fmt.Sprintf("launchtl load \"%s\"", p)).Exec()
+	cmd.New(fmt.Sprintf("launchtl start \"%s\"", p)).Exec()
+
+	println("tron is installed and will report daily.")
 }
