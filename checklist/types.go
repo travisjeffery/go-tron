b55b3849ad26f6895f8a8ee94891583592810005
Travis Jeffery <tj@travisjeffery.com>
2014-08-06 04:27:29 -0400

    Add color

--- checklist/types.go
+++ checklist/types.go
@@ -1,6 +1,6 @@
 package checklist
 
-import "fmt"
+import "github.com/fatih/color"
 
 type Test struct {
 	description string
@@ -25,15 +25,17 @@ type Runner struct {
 func (r *Runner) Run() (successes, failures []string) {
 	successes = []string{}
 	failures = []string{}
+	successColor := color.New(color.FgGreen)
+	failureColor := color.New(color.FgRed).Add(color.Bold)
 
 	for _, s := range r.Suites {
 		for _, c := range s.Tests {
 			if c.run() {
 				successes = append(successes, c.description)
-				fmt.Println(":white_test_mark:", c.description)
+				successColor.Println(" ✓", c.description)
 			} else {
 				failures = append(failures, c.description)
-				fmt.Println(":warning:", c.description)
+				failureColor.Println(" ✗", c.description)
 			}
 		}
 	}
